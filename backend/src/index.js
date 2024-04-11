8d099138431962fae33ad0b7a780d1c9f2c633ed
Siddhant <sidsaiba2703@gmail.com>
2024-04-11 12:33:21 +0530

    file structure

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+import { app } from "./app.js"
+
+dotenv.config({
+    path:'./.env'
+})
+
+connectDB()
+.then(()=>{
+    console.log(1);
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`server is listening on port ${process.env.PORT || 3000}`);        
+    })
+    app.on('error',(err)=>{
+        console.log("error in app: ", err);
+        throw err;        
+    })
+})
+.catch((err)=>{
+    console.log("DB connection error: ",err);
+})
